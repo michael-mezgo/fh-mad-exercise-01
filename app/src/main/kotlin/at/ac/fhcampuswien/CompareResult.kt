06f14dd46198c139e3569afbe00166705ab9c7e6
leonardo1710 <leon.freudenthaler@fh-campuswien.ac.at>
2024-02-19 19:34:43 +0100

    fixed incorrect n:m test cases and updated CompareResult accordingly.

--- app/src/main/kotlin/at/ac/fhcampuswien/CompareResult.kt
+++ app/src/main/kotlin/at/ac/fhcampuswien/CompareResult.kt
@@ -4,13 +4,13 @@ package at.ac.fhcampuswien
  *
  * Look at the toSting() and use it in your output
  *
- * @param m The number of digits guessed correctly (regardless of their position)
- * @param n The number of digits guessed correctly and in the correct position.
+ * @param n The number of digits guessed correctly (regardless of their position)
+ * @param m The number of digits guessed correctly and in the correct position.
  * @constructor creates an instance of the class.
- * @see ld.main
  */
-data class CompareResult(val m: Int, val n: Int){
+data class CompareResult(val n: Int, val m: Int){
     override fun toString(): String {
-        return "Output: $m:$n"
+        return "Output: $n:$m"
     }
 }
+
